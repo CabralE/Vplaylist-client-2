b2a1dc5f93d3ac751ea600047afdd60aa06f3fc8
ed <cabrale234@gmail.com>
2022-11-22 21:32:04 -0500

    refactored to simplify signup. it works now

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import NavBar from "./components/NavBar/NavBar.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import "./App.css";
+import Home from "./screens/Home";
+import SignUp from "./screens/SignUp.jsx";
+import SignIn from "./screens/SignIn.jsx";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/screens/SignIn.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { signIn } from "../services/users";
+function SignIn() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  let navigate = useNavigate();
+
+  function handleChange(event) {
+    const newState = { ...user };
+    newState[event.target.name] = event.target.value;
+    setUser(newState);
+  }
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await signIn(user);
+    navigate("/sign-in", { replace: true });
+  };
+  return (
+    <>
+      This is the Sign-in Page
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="email"
+          name="email"
+          value={user.email}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="password"
+          name="password"
+          value={user.password}
+          onChange={handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+}
+
+export default SignIn;
