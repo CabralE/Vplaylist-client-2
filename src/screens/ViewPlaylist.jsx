c2eff85c5b564c701bc2c11c5b043ac15e6fbfda
ed <cabrale234@gmail.com>
2022-11-26 19:32:09 -0500

    playlist fetching

--- src/screens/ViewPlaylist.jsx
+++ src/screens/ViewPlaylist.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { getPlaylist } from "../services/playlists";
 // import Loading from "../components/Loading/Loading";
 
@@ -16,10 +16,32 @@ function ViewPlaylist() {
     fetchPlaylist();
   }, [id]);
 
+  const loaded = () => {
+    console.log("playlist in viewPlaylist", playlist);
+    const playlistData = playlist.videos.map((video, index) => (
+      <div key={video._id} class="video">
+        <Link to={`${video.videoUrl}`}>
+          <h1>{video.videoName}</h1>
+        </Link>
+        <iframe
+          width="560"
+          height="315"
+          src={video.videoUrl}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    ));
+
+    return <div>{playlistData}</div>;
+  };
+
   return (
     <>
       <h1>View the playlist here</h1>
-      {playlist ? JSON.stringify(playlist) : "test"}
+      {playlist ? loaded() : "test"}
     </>
   );
 }
