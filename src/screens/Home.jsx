8638766c7c1fffa81a25606f50b18c59c33b76a7
ed <cabrale234@gmail.com>
2022-12-07 18:09:14 -0500

    navbar changes as user logs in and out

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,4 +1,18 @@
+import { useState, useEffect } from "react";
+
 function Home() {
+  const [userLoggedIn, setuserLoggedIn] = useState(false);
+
+  const fetchData = async () => {
+    const item = await JSON.parse(localStorage.getItem("name"));
+    if (item) setuserLoggedIn(!userLoggedIn);
+    console.log("fetchedData!", `${item}`, { userLoggedIn });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [setuserLoggedIn]);
+
   return <div>This is the Home Page!</div>;
 }
 
